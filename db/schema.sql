83683dd258272b88a9b031a9598fc9c58853baf5
AveryNewhart <newhartreeceavery@gmail.com>
2023-02-01 21:53:36 -0500

    created content for schema file

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,42 @@
 -- create three tables
     -- data for the tables/columns in the mod chal guideline
 
--- DROP DATABASE
--- CREATE DATABASE
--- USE DATABASE
+-- creating database
+CREATE DATABASE employer_db;
+
+-- using the database created
+USE employer_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, -- auto increment the primary key
+    name VARCHAR(30), -- hold department name
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, -- auto increment the primary key,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT, -- hold reference to department the role belongs to
+    FOREIGN KEY (department_id) -- getting the primary key from department table and making it a foreign key.
+    REFERENCES department(id) -- tells it to refer to the department table to get the id. 
+    ON DELETE SET NULL -- sets everything to NULL if content from parent table is deleted.
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT, -- hold reference for employee roll
+    manager_id INT, -- hold reference to another employee that is the manager of the current employee (null if the employee has no manager)
+    FOREIGN KEY (role_id) -- after employee!! so this will refer to the key in the role table. 
+    REFERENCES role(id) -- refering to the role table and the id inside(the foregin key that was go by gettin gthe primary key earlier.)
+    ON DELETE SET NULL -- sets everything to NULL if content from parent table is deleted.
+);
+
+
+
+
+
+-- A foreign key with "set null on delete" means that if a record in the parent table is deleted, then the 
+-- corresponding records in the child  table will have the 
+-- foreign key fields set to NULL. The records in the child table will not be deleted in SQL Server.
